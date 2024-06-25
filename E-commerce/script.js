74bdb40260f8a3d411b3330b91764a9e52761ad2
Sri0410 <149194402+Sri0410@users.noreply.github.com>
2024-06-25 10:39:05 +0530

    Add files via upload

---
+++ E-commerce/script.js
@@ -0,0 +1,9 @@
+var side_navbar = document.querySelector(".side_navbar");
+
+function showsidenavbar() {
+    side_navbar.style.left = "0";
+}
+
+function closesidenavbar() {
+    side_navbar.style.left = "100px";
+}
